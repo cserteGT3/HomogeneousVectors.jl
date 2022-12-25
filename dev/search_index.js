76a3378b78e6ec458db19ac502300369fe8eb458
Documenter.jl <documenter@juliadocs.github.io>
2022-12-25 08:33:11 +0000

    build based on 0e38847

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HomogeneousVectors","category":"page"},{"location":"#HomogeneousVectors","page":"Home","title":"HomogeneousVectors","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HomogeneousVectors.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HomogeneousVectors]","category":"page"}]
+}
